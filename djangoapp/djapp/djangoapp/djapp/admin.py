05b7cb8d26d99d5084735759c5154f20c13ad752
Semen <165212317+SemenOlkov@users.noreply.github.com>
2025-01-15 01:51:40 +0500

    Delete djangoapp/djapp/djangoapp directory

--- djangoapp/djapp/djangoapp/djapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import SalaryData
-
-@admin.register(SalaryData)
-class SalaryDataAdmin(admin.ModelAdmin):
-    list_display = ('year', 'average_salary')
-    list_editable = ('average_salary',)
-    search_fields = ('year',)
